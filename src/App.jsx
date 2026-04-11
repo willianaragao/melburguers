2c0dd580c8f20abcdc8cd50362667a0b4544616e
willianaragao <willianao84@gmail.com>
2026-04-11 19:59:17 -0300

    UI: Remoção do nome redundante na bio

--- src/App.jsx
+++ src/App.jsx
@@ -56,7 +56,6 @@ const App = () => {
 
         <div className="bio-area">
           <div className="bio-text">
-            MELBURGERS 🍔 <br />
             Sabor que conquista na primeira mordida ✨ <br />
             <Truck size={14} style={{ verticalAlign: 'middle', marginRight: '4px' }} /> 
             Somente Delivery <br />
